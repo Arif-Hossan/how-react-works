f64a30b78d6fc3501508ac9de2e00110488cc795
Arif Hossan <arif.hossan.eu@gmail.com>
2023-04-03 02:29:52 +0600

    class component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import logo from './logo.svg';
 import './App.css';
 import Device from './components/Device/Device';
 import Watch from './components/Watch/Watch';
+import Tablet from './components/Tablet/Tablet';
 
 function App() {
   return (
     <div className="App">
       <Device name="Uphone" price="13000" />
       <Watch></Watch>
+      <Tablet></Tablet>
+
     </div>
   );
 }
